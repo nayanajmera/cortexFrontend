e5127deecf303594b0841215f32d391fd1cf5ca1
Nayan Ajmera <nayanajmera1234@gmail.com>
2026-04-16 22:13:31 +0530

    Fixed bug. Forgot to change API endpoint in previous commit, rectified it in this commit.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://cortex-bb7r.onrender.com/api',
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://cortex-bb7r.onrender.com/api',
+  // baseURL: 'http://localhost:8080/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
